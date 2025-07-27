74b0c7aca54df416c615a212cb2b6ea7bf7af5eb
Berkant Gunes <gunesberkant96@gmail.com>
2025-07-27 12:22:13 +0300

    Add files via upload

---
+++ CustomerValidator.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLayer.FluentValidation
+{
+    public class CustomerValidator : AbstractValidator<Customer>
+    {
+        public CustomerValidator()
+        {
+            RuleFor(x=>x.CustomerCity).NotEmpty().WithMessage("City can not be blank!");
+            RuleFor(x=>x.CustomerName).NotEmpty().WithMessage("Customer Name can not be blank!");
+        }
+    }
+}
---
+++ ProductValidator.cs
@@ -0,0 +1,23 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLayer.FluentValidation
+{
+    public class ProductValidator : AbstractValidator<Product>
+    {
+        public ProductValidator() // constructor
+        {
+            // Rules
+            RuleFor(x => x.ProductName).NotEmpty().WithMessage("Product Name can not be blank!");
+            RuleFor(x => x.ProductName).MinimumLength(3).WithMessage("Product Name at least 3 Characters!");
+            RuleFor(x => x.Stock).NotEmpty().WithMessage("Product Stock can not be blank!");
+            RuleFor(x => x.Price).NotEmpty().WithMessage("Product Price can not be blank!");
+
+        }
+    }
+}
