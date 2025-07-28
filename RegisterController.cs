a954c0cfea3e2d174d6a896e5b3a5f18ae86517a
Berkant Gunes <gunesberkant96@gmail.com>
2025-07-28 08:56:59 +0300

    Add files via upload

---
+++ RegisterController.cs
@@ -0,0 +1,53 @@
+﻿using Demo_Product.Models;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Demo_Product.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(UserRegisterViewModel model)
+        {
+            // Atamalari yapalim
+            AppUser appUser = new AppUser()
+            {
+                Name = model.Name,
+                Surname = model.Surname,
+                UserName = model.UserName,
+                Email = model.Email
+            };
+
+            if(model.Password == model.ConfirmPassword)
+            {
+                var result = await _userManager.CreateAsync(appUser, model.Password);
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("Index","Login"); // LoginControllerda Index e yonlendirir
+                }
+                else
+                {
+                    foreach(var item in result.Errors)
+                    {
+                        ModelState.AddModelError("",item.Description);
+                    }
+                }
+            }
+            return View();
+        }
+    }
+}
