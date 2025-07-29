26f51c7e3c79d37c4ea7ae9f8011b4af2eb0fc00
Berkant Gunes <gunesberkant96@gmail.com>
2025-07-29 11:18:41 +0300

    Add files via upload

---
+++ CustomIdentityValidator.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Demo_Product.Models
+{
+    public class CustomIdentityValidator:IdentityErrorDescriber
+    {
+        public override IdentityError PasswordTooShort(int length)
+        {
+            return new IdentityError()
+            {
+                Code = "Password Too Short",
+                Description = "Password Must Be At Least 6 Characters"
+            };
+        }
+        public override IdentityError PasswordRequiresUpper()
+        {
+            return new IdentityError()
+            {
+                Code="PasswordRequiresUpper",
+                Description = "Password contains at least 1 Upper Case Letter"
+            };
+        }
+        public override IdentityError PasswordRequiresLower()
+        {
+            return new IdentityError()
+            {
+                Code="PasswordRequiresLower",
+                Description="Password contains at least 1 Lower Case Letter"
+            };
+        }
+        public override IdentityError PasswordRequiresNonAlphanumeric()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresNonAlphanumeric",
+                Description = "Password contains at least 1 alphanumeric character"
+            };
+        }
+    }
+}
