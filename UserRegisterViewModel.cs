52959a131491b25405684bfb8e26855b9fc6d8ab
Berkant Gunes <gunesberkant96@gmail.com>
2025-07-29 11:19:14 +0300

    Add files via upload

---
+++ UserRegisterViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Demo_Product.Models
+{
+    public class UserRegisterViewModel
+    {
+        // Attributes and Props
+        [Required(ErrorMessage = "Please enter name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter surname")]
+        public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Please enter username")]
+        public string UserName { get; set; }
+
+        public string Gender { get; set; }
+
+        [Required(ErrorMessage = "Please enter email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter password")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Please re-enter password")]
+        [Compare("Password", ErrorMessage ="Password and Confirm Password must the same")]
+        public string ConfirmPassword { get; set; }
+    }
+}
